b9e51b432d7b797e72e161e961bed9fff340d0a4
acatxnamedvirtue <tsb252@gmail.com>
2015-08-13 15:00:42 -0400

    Add the bonus methods

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,10 +5,44 @@ class Question < ActiveRecord::Base
   has_many :answer_choices,
     class_name: "AnswerChoice",
     foreign_key: :question_id,
-    primary_key: id
+    primary_key: :id
 
   belongs_to :poll,
     class_name: "Poll",
     foreign_key: :poll_id,
     primary_key: :id
+
+  has_many :responses,
+    through: :answer_choices,
+    source: :responses
+
+  def results
+    # choices = answer_choices.includes(:responses)
+    # result = {}
+    # choices.each do |choice|
+    #   result[choice.text] = choice.responses.count
+    # end
+    # result
+
+    # sql = <<-SQL
+    #   SELECT
+    #     answer_choices.*, COUNT(responses.id) AS responses_count
+    #   FROM
+    #     answer_choices
+    #   LEFT JOIN
+    #     responses
+    #     ON answer_choices.id = responses.answer_choice_id
+    #   WHERE
+    #     answer_choices.question_id = #{self.id}
+    #   GROUP BY
+    #     answer_choices.id
+    # SQL
+
+    choices_with_counts = answer_choices
+      .select('answer_choices.*, COUNT(responses.id) AS responses_count')
+      .joins('LEFT JOIN responses ON answer_choices.id = responses.answer_choice_id')
+      .group('answer_choices.id')
+
+    choices_with_counts.map { |choice| [choice.text, choice.responses.count] }.to_h
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,47 @@ class User < ActiveRecord::Base
     class_name: "Response",
     foreign_key: :user_id,
     primary_key: :id
-    
+
+  def completed_polls
+    # sql = <<-SQL
+    #   SELECT
+    #     polls.*
+    #   FROM
+    #     polls
+    #   JOIN
+    #     questions ON questions.poll_id = polls.id
+    #   JOIN
+    #     answer_choices ON questions.id = answer_choices.question_id
+    #   LEFT JOIN (
+    #       SELECT
+    #         responses.*
+    #       FROM
+    #         responses
+    #       WHERE
+    #         responses.user_id = 3
+    #     ) AS user_responses ON answer_choices.id = user_responses.answer_choice_id
+    #   GROUP BY
+    #     polls.id
+    #   HAVING
+    #     COUNT(questions.id) = COUNT(user_responses)
+    # SQL
+
+    Poll.all
+      .select("polls.*")
+      .joins(:questions)
+      .joins("JOIN answer_choices ON questions.id = answer_choices.question_id")
+      .joins("LEFT JOIN (SELECT responses.* FROM responses WHERE responses.user_id = #{self.id}) AS user_responses ON answer_choices.id = user_responses.answer_choice_id")
+      .group("polls.id")
+      .having("COUNT(questions.id) = COUNT(user_responses)")
+  end
+
+  def uncompleted_polls
+    Poll.all
+      .select("polls.*")
+      .joins(:questions)
+      .joins("JOIN answer_choices ON questions.id = answer_choices.question_id")
+      .joins("LEFT JOIN (SELECT responses.* FROM responses WHERE responses.user_id = #{self.id}) AS user_responses ON answer_choices.id = user_responses.answer_choice_id")
+      .group("polls.id")
+      .having("COUNT(questions.id) != COUNT(user_responses)")
+  end
 end
