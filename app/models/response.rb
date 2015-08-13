3c7d5d87f197a7d8c3d04b53b3138430574960cb
Tyler_and_Arthur <Tyler_n_Arthur@appacademy.io>
2015-08-13 15:00:42 -0400

    Add the bonus methods

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,6 +1,8 @@
 class Response < ActiveRecord::Base
   validates :user_id, presence: true
   validates :answer_choice_id, presence: true
+  validate :respondent_has_not_already_answered_question
+  validate :author_cannot_respond_to_own_poll
 
   belongs_to :answer_choice,
     class_name: "AnswerChoice",
@@ -11,4 +13,106 @@ class Response < ActiveRecord::Base
     class_name: "User",
     foreign_key: :user_id,
     primary_key: :id
+
+  has_one :question,
+    through: :answer_choice,
+    source: :question
+
+  def sibling_responses
+    # question.responses.where("CASE WHEN ? IS NOT NULL THEN responses.id != ? ELSE TRUE END", self.id, self.id)
+    sql = <<-SQL
+      SELECT
+        responses.*
+      FROM
+        answer_choices AS ac1
+      JOIN
+        questions
+        ON questions.id = ac1.question_id
+      JOIN
+        answer_choices AS ac2
+        ON ac2.question_id = questions.id
+      JOIN
+        responses
+        ON responses.answer_choice_id = ac2.id
+      WHERE
+        ac1.id = ?
+        AND CASE WHEN ? IS NOT NULL THEN responses.id != ? ELSE TRUE END
+
+        SELECT responses.*
+        FROM "responses"
+        INNER JOIN "answer_choices" ON "answer_choices"."id" = "responses"."answer_choice_id"
+        INNER JOIN "questions" ON "questions"."id" = "answer_choices"."question_id"
+        JOIN answer_choices AS ac1 ON questions.id = ac1.question_id
+        JOIN answer_choices AS ac2 ON ac2.question_id = questions.id
+        JOIN responses ON responses.answer_choice_id = ac2.id WHERE (ac1.id = 1 AND CASE WHEN 1 IS NOT NULL THEN responses.id != 1 ELSE TRUE END)
+
+        SELECT "responses".*
+          FROM "responses"
+        INNER JOIN
+          "answer_choices" ON "answer_choices"."id" = "responses"."answer_choice_id"
+        INNER JOIN
+          "questions" ON "questions"."id" = "answer_choices"."question_id"
+        JOIN
+          answer_choices AS ac1 ON questions.id = ac1.question_id
+        JOIN
+          answer_choices AS ac2 ON ac2.question_id = questions.id
+        WHERE
+          (ac1.id = 1 AND CASE WHEN 1 IS NOT NULL THEN responses.id != 1 ELSE TRUE END)
+
+        SELECT
+          "responses".*
+        FROM
+          "responses"
+        INNER JOIN
+          "answer_choices" ON "answer_choices"."id" = "responses"."answer_choice_id"
+        INNER JOIN
+          "questions" ON "questions"."id" = "answer_choices"."question_id"
+        JOIN
+          answer_choices AS ac1 ON questions.id = ac1.question_id
+        WHERE
+          (ac1.id = 1 AND CASE WHEN 1 IS NOT NULL THEN responses.id != 1 ELSE TRUE END)
+
+    SQL
+
+    results = Response
+      .joins("JOIN answer_choices AS ac1 ON questions.id = ac1.question_id")
+      .joins(:question)
+      .where("ac1.id = ? AND CASE WHEN ? IS NOT NULL THEN responses.id != ? ELSE TRUE END", self.answer_choice_id, self.id, self.id)
+      # .joins("JOIN answer_choices AS ac2 ON ac2.question_id = questions.id")
+      # .joins("JOIN responses AS r1 ON r1.answer_choice_id = ac2.id")
+      # .where("ac1.id = ? AND CASE WHEN ? IS NOT NULL THEN r1.id != ? ELSE TRUE END", self.answer_choice_id, self.id, self.id).distinct
+  end
+
+  private
+  def respondent_has_not_already_answered_question
+    if sibling_responses.any? {|response| response.user_id == self.user_id}
+      errors[:user_id] << "has already answered this question."
+    end
+  end
+
+  def author_cannot_respond_to_own_poll
+    # sql = <<-SQL
+    #   SELECT
+    #     polls.author_id
+    #   FROM
+    #     answer_choices
+    #   JOIN
+    #     questions
+    #     ON questions.id = answer_choices.question_id
+    #   JOIN
+    #     polls
+    #     ON polls.id = questions.poll_id
+    #   WHERE
+    #     answer_choices.id = 5;
+    # SQL
+
+    result = Poll.select("polls.author_id")
+              .joins(:questions)
+              .joins("JOIN answer_choices ON questions.id = answer_choices.question_id")
+              .where("answer_choices.id = #{self.answer_choice_id}")
+
+    if result.first.author_id = self.user_id
+      errors[:user_id] << "cannot respond to own poll."
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# users = User.create!([{user_name: "Tyler"}, {user_name: "Arthur"}])
+# polls = Poll.create!([{title: "Poll1", author_id: 1}, {title: "Poll2", author_id: 2}])
+questions = Question.create!([{poll_id: 1, text: "Question1"},
+                             {poll_id: 1, text: "Question2"},
+                             {poll_id: 2, text: "Question3"},
+                             {poll_id: 2, text: "Question4"}
+                             ])
+answer_choices = AnswerChoice.create!([{question_id: 1, text: "AnswerChoice1"},
+                                      {question_id: 2, text: "AnswerChoice2"},
+                                      {question_id: 3, text: "AnswerChoice3"},
+                                      {question_id: 4, text: "AnswerChoice4"}
+                                      ])
+responses = Response.create!([{user_id: 1, answer_choice_id: 1},
+                              {user_id: 1, answer_choice_id: 2},
+                              {user_id: 1, answer_choice_id: 3},
+                              {user_id: 1, answer_choice_id: 4},
+                              {user_id: 2, answer_choice_id: 1},
+                              {user_id: 2, answer_choice_id: 2},
+                              {user_id: 2, answer_choice_id: 3},
+                              {user_id: 2, answer_choice_id: 4}
+                            ])
